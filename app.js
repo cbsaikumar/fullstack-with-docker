09b26ded7ace91c691ab6efe90c6952bef2e009c
Bhargava Chittapudi <Bhargava_Chittapudi@epam.com>
2020-06-05 21:00:15 +0530

    changed internal port to 8080

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8080
 
 app.use('/', (req, res) => res.json({
     meta: {
