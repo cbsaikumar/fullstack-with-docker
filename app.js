450ec621dea00bd421704063efa93fecc6b4da69
Bhargava Chittapudi <Bhargava_Chittapudi@epam.com>
2020-06-06 11:56:32 +0530

    added 2 more routes

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const app = express()
 
 const port = process.env.PORT || 8080
 
-app.use('/', (req, res) => res.json({
+app.get('/', (req, res) => res.json({
     meta: {
         status: true,
         message: 'Bhargava welcomes you to the app!',
         code: 200
     }
 }))
+
+app.get('/login/facebook', (req, res) => res.json({
+    meta: {
+        status: true,
+        message: 'It is facebook oAuth login',
+        code: 200
+    }
+}))
+
+app.get('**', (req, res) => res.redirect(302, '/'))
 
 app.listen(port, console.log(`app listening on ${port}`))
 
